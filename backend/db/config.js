18fc17b8b057d5c73ecdd45ce055d4abef55fb14
parthiv8848 <124236867+parthiv8848@users.noreply.github.com>
2023-09-17 15:07:33 +0530

    Update config.js

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,2 +1,2 @@
 const mongoose= require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/e-commerce")
+mongoose.connect("mongodb+srv://parthivkevadiya:pOZUnSyZusT1hN09@cluster0.rzulcsn.mongodb.net/")
