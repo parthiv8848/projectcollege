399ea1d8e689e1306b1bb90ec2dffc360e42f880
parthiv8848 <124236867+parthiv8848@users.noreply.github.com>
2023-10-19 10:09:38 +0530

    Update SignUp.js

--- front-end/src/components/SignUp.js
+++ front-end/src/components/SignUp.js
@@ -23,7 +23,7 @@ const SignUp = () => {
             }
         });
         result = await result.json();
-        console.warn(result);
+        // console.warn(result);
         localStorage.setItem("user", JSON.stringify(result.result))
         localStorage.setItem("token", JSON.stringify(result.auth))
 
